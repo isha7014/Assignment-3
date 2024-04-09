d638fb0001650fe57a9f12b97af0d3a4c38befa0
isha7014 <157243360+isha7014@users.noreply.github.com>
2024-04-08 23:53:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function playSound(soundFile) {
+    var audio = document.getElementById("cat-sound");
+    audio.src = soundFile;
+    audio.play();
+  }
+  
+  function playCat1Sound() {
+    playSound('mixkit-cartoon-little-cat-meow-91 (1).wav');
+  }
+  
+  function playCat2Sound() {
+    playSound('mixkit-angry-cartoon-kitty-meow-94.wav');
+  }
+  
+  function playCat3Sound() {
+    playSound('mixkit-little-cat-pain-meow-87.wav');
+  }
+  function playCat3Sound() {
+    playSound('mixkit-domestic-cat-hungry-meow-45.wav');
+  }
+  function playCat3Sound() {
+    playSound('mixkit-big-wild-cat-scary-roar-88.wav');
+  }
+  
